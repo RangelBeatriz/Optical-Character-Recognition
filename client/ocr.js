acd4dbb89aa82551582c8a4288973035bc366ce3
Beatriz Rangel <beatriz.rangel@dadoteca.com.br>
2025-09-17 17:23:57 -0300

    feat(client): add initial ocr.js with canvas drawing functions

---
+++ client/ocr.js
@@ -0,0 +1,45 @@
+var ocrDemo = {
+    CANVAS_WIDTH: 200,
+    TRANSLATED_WIDTH: 20,
+    PIXEL_WIDTH: 10,
+
+
+    drawGrid: function(ctx) {
+        for (var x = this.PIXEL_WIDTH, y = this.PIXEL_WIDTH;
+                x < this.CANVAS_WIDTH; x += this.PIXEL_WIDTH,
+                y += this.PIXEL_WIDTH) {
+
+            ctx.strokeStyle = this.BLUE;
+            ctx.beginPath();
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, this.CANVAS_WIDTH);
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(0, y);
+            ctx.lineTo(this.CANVAS_WIDTH, y);
+            ctx.stroke();
+        }
+    },
+
+    onMouseMove: function(e, ctx, canvas) {
+        if (!canvas.isDrawing) {
+            return;
+        }
+
+        this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    },
+
+    onMouseDown: function(e, ctx, canvas) {
+        canvas.isDrawing = true;
+        this.fillSquare(ctx, e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop);
+    },
+
+    onMouseUp: function(e) {
+        canvas.isDrawing = false;
+    },
+
+
+}
+
+
